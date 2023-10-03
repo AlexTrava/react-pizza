ffbb1e019cd136c09c2abd957e34d89b2e5af8fb
Aleksey <aleksei.travnov@mail.ru>
2023-10-04 00:22:08 +0400

    add categories active state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import styles from "./Header.module.scss";
+import logo from "../../img/pizza-logo.svg";
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.container}>
         <div className={styles.header__logo}>
-          <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+          <img width="38" src={logo} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>
