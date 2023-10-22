e6bca6ebe3c2397b7df9df9a0c8cdc55b3424908
Aleksey <aleksei.travnov@mail.ru>
2023-10-23 00:04:18 +0400

    update sortSlice

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import uniqId from "lodash.uniqueid";
 import axios from "axios";
+import qs from "qs";
 
 import Categories from "../../components/Categories/Categories";
 import Sort from "../../components/Sort/Sort";
@@ -15,10 +17,10 @@ import { setPageCurrent } from "../../redux/slices/paginationSlice";
 const URL_ITEMS = "https://651e965944a3a8aa4768a0da.mockapi.io/items";
 
 const Home = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [items, setItems] = useState([]);
   const [isLoading, setLoading] = useState(true);
-  // const [currentPage, setCurrentPage] = useState(1);
 
   const categoryId = useSelector((state) => state.category.categoryId);
   const typeSort = useSelector((state) => state.sort.activeSortType);
@@ -28,6 +30,13 @@ const Home = () => {
   const onChangePage = (number) => {
     dispatch(setPageCurrent(number));
   };
+
+  useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      console.log(params, "params");
+    }
+  }, []);
 
   useEffect(() => {
     setLoading(true);
@@ -47,6 +56,16 @@ const Home = () => {
       .catch((e) => console.log(e));
     window.scrollTo(0, 0);
   }, [categoryId, typeSort, searchValue, currentPage]);
+
+  useEffect(() => {
+    const queryString = qs.stringify({
+      sortProperty: typeSort,
+      categoryId: categoryId,
+      currentPage: currentPage,
+    });
+    console.log(queryString, "string");
+    navigate(`?${queryString}`);
+  }, [categoryId, typeSort, currentPage]);
 
   const skeletons = [...new Array(6)].map((_, index) => (
     <Skeleton key={index} />
--- src/redux/slices/sortSlice.js
+++ src/redux/slices/sortSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   activeSortId: 0,
-  activeSortType: "",
+  activeSortType: "rating",
 };
 
 const sortSlice = createSlice({
